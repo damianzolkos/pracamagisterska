1d62b9c5550bdbdf83410fe97a2d2aafe6faa9e1
630020 <damianzolkos@gmail.com>
2020-05-27 19:27:22 +0200

    final? may be

--- esp8266/mgr_esp8266_1/magisterka_esp8266_1/magisterka_esp8266_1.ino
+++ esp8266/mgr_esp8266_1/magisterka_esp8266_1/magisterka_esp8266_1.ino
@@ -12,13 +12,16 @@ WiFiManager wifiManager;
 
 const char* serverAddress = "http://192.168.1.11/pracamagisterska/backend/receive.php"; // adres skryptu odbierającego dane
 
+unsigned long zapamietanyCzas2 = 0;
+uint8_t trybGrzalki, poziomWilgotnosci;
+
 int airPeriod = 30;
 
 SdsDustSensor sds(Serial);
 DHTesp dht;
 
 #define SEALEVELPRESSURE_HPA (1013.25)
-#define grzalka D5
+#define grzalka D6
 Adafruit_BME280 bme;
 float pm25, pm10;
 float temperatureBME, humidityBME, pressureBME, altitudeBME;
@@ -32,7 +35,7 @@ void configModeCallback (WiFiManager *myWiFiManager) {
 void setup() {
   Serial.begin(9600);
   sds.begin();
-
+  pinMode(grzalka, OUTPUT);
   wifiManager.setAPCallback(configModeCallback);
   wifiManager.autoConnect("Czujnik warunków pogodowych");
 
@@ -52,29 +55,51 @@ void loop() {
   humidityBME = bme.readHumidity();
   pressureBME = bme.readPressure() / 100.0F;
   altitudeBME = bme.readAltitude(SEALEVELPRESSURE_HPA);
-
-  Serial.println("--------------------------------------------");
-  Serial.print("Wilgotnosc rurka: ");
+  Serial.println("");
+  Serial.println("");
+    Serial.println("----------------------------------------------------------------------");
+  Serial.print("POMIAR ID: ");
+  Serial.println(millis());
+    Serial.println("----------------------------------------------------------------------");
+  Serial.print("Temperatura BME: ");
+  Serial.print(temperatureBME);
+  Serial.println("");
+  Serial.print("Wilgotnosc BME: ");
+  Serial.print(humidityBME);
+  Serial.println("");
+  Serial.print("Ciśnienie BME: ");
+  Serial.print(pressureBME);
+  Serial.println("");
+    Serial.println("----------------------------------------------------------------------");
+  Serial.print("Wilgotnosc DHT: ");
   Serial.print(humidityDHT);
   Serial.println("");
-  Serial.print("Temperatura rurka: ");
+  Serial.print("Temperatura DHT: ");
   Serial.print(temperatureDHT);
   Serial.println("");
   Serial.print("Stan grzalki: ");
-  if (humidityDHT < 50) {
-        digitalWrite(grzalka, HIGH);
+  if (humidityDHT >= 55) {
+    digitalWrite(grzalka, HIGH);
     Serial.print("ON");
   } else {
-        digitalWrite(grzalka, LOW);
+    digitalWrite(grzalka, LOW);
     Serial.print("OFF");
   }
   Serial.println("");
-  Serial.println("--------------------------------------------");
+    Serial.println("----------------------------------------------------------------------");
 
   PmResult pm = sds.readPm();
   if (pm.isOk()) {
     pm25 = pm.pm25;
     pm10 = pm.pm10;
+
+    Serial.print("PM 2,5: ");
+    Serial.print(pm25);
+    Serial.println(" ug/m3 ");
+    Serial.print("PM 10: ");
+    Serial.print(pm10);
+    Serial.println(" ug/m3 ");
+    Serial.println("----------------------------------------------------------------------");
 
     String response;
     DynamicJsonDocument doc(1024);
@@ -85,8 +110,7 @@ void loop() {
     doc["pm25"] = pm25;
     doc["pm10"] = pm10;
 
-    serializeJson(doc, response); 
-    Serial.println(response);
+    serializeJson(doc, response);
 
     HTTPClient http;                                              // Declare object of class HTTPClient
     http.begin(serverAddress);                                    // Specify request destination
@@ -94,13 +118,13 @@ void loop() {
     http.addHeader("Access-Control-Allow-Origin", "*");
     int httpCode = http.POST(response);                           // Send the request
     String payload = http.getString();                            // Get the response payload
-    Serial.println(httpCode);   //Print HTTP return code
-    Serial.println(payload);    //Print request response payload
     http.end();                                                   // Close connection
   } else {
     Serial.print("Could not read values from sensor, reason: ");
     Serial.println(pm.statusToString());
+    Serial.println("----------------------------------------------------------------------");
   }
+
 
   delay(5000);
 }
